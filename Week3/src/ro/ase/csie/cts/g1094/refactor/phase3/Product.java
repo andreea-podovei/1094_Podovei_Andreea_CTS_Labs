d67a28ea7ee4858471299f97e65a9303523256d0
andreea-podovei <72200669+andreea-podovei@users.noreply.github.com>
2021-03-16 15:26:51 +0200

    Week 4 part 2 (final commit)

--- Week3/src/ro/ase/csie/cts/g1094/refactor/phase3/Product.java
+++ Week3/src/ro/ase/csie/cts/g1094/refactor/phase3/Product.java
@@ -1,19 +1,43 @@
 package ro.ase.csie.cts.g1094.refactor.phase3;
 
+import java.nio.channels.UnsupportedAddressTypeException;
+
 import ro.ase.csie.cts.g1094.refactor.exceptions.InvalidAgeException;
 import ro.ase.csie.cts.g1094.refactor.exceptions.InvalidPriceException;
 import ro.ase.csie.cts.g1094.refactor.phase3.services.MarketingServiceInterface;
+import ro.ase.csie.cts.g1094.refactor.phase3.services.ValidatorServiceInterface;
+import ro.ase.csie.cts.g1094.refactor.phase3.test.TestProduct;
 
 public class Product {
 
 	MarketingServiceInterface mkService = null;
-	
-	public Product(MarketingServiceInterface mkService) {
+	ValidatorServiceInterface validator = null;
+	public Product(MarketingServiceInterface mkService, ValidatorServiceInterface validator) {
 		/*if(mkService == null) {
 			throw new NullPointerException();
 		}
 		this.mkService = mkService;*/
 		this.setMarketingService(mkService);
+		this.validator = validator;
+	}
+	
+	//version 4 - use the global service collection
+	public Product() {
+		//dependency injection based on the global services collection
+		for(Object service : TestProduct.services) {
+			if(service instanceof MarketingServiceInterface) {
+				this.setMarketingService((MarketingServiceInterface)service);
+			}
+			if(service instanceof ValidatorServiceInterface) {
+				this.validator = (ValidatorServiceInterface) service;
+			}
+		}
+		if(this.mkService == null) {
+			throw new UnsupportedOperationException();
+		}
+		if(this.validator == null) {
+			throw new UnsupportedOperationException();
+		}
 	}
 	
 	//optional - based on design specs
@@ -42,12 +66,8 @@ public class Product {
 	
 	public float computePriceWithDiscount(ProductType productType, float price, int accountAge) throws InvalidPriceException, InvalidAgeException
 	  {
-		if(price <= 0) {
-			throw new InvalidPriceException();
-		}
-		if(accountAge < 0) {
-			throw new InvalidAgeException();
-		}
+		validator.validatePrice(price);
+		validator.validateAge(accountAge);
 		
 	    float fidelityDiscount =
 	    		(productType == ProductType.NEW) ? 0 : mkService.getFidelityDiscount(accountAge);
