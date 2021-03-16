362227e96c777e90e53f88389af416a62fb5c955
andreea-podovei <72200669+andreea-podovei@users.noreply.github.com>
2021-03-16 14:34:15 +0200

    Week 4 part 1 (before break) + small change to week 3 phase 2 ProductType class

--- Week3/src/ro/ase/csie/cts/g1094/refactor/phase3/ProductType.java
+++ Week3/src/ro/ase/csie/cts/g1094/refactor/phase3/ProductType.java
@@ -1,4 +1,4 @@
-package ro.ase.csie.cts.g1094.refactor.phase2;
+package ro.ase.csie.cts.g1094.refactor.phase3;
 
 public enum ProductType {
 	NEW(0), DISCOUNTED(0.1f), LIMITED_STOCK(0.25f), LEGACY(0.35f);
