71c6c4711ccaf2fb44a4549c4784f54bc4b2c703
andreea-podovei <72200669+andreea-podovei@users.noreply.github.com>
2021-04-13 15:38:13 +0300

    Week 8 - Adapter and Decorator implementations (final commit)

--- Week 8 - Facade, Adapter and Decorator/src/ro/ase/csie/cts/g1094/dp/adapter/disney/DisneyActions.java
+++ Week 8 - Facade, Adapter and Decorator/src/ro/ase/csie/cts/g1094/dp/adapter/disney/DisneyActions.java
@@ -2,7 +2,10 @@ package ro.ase.csie.cts.g1094.dp.adapter.disney;
 
 public interface DisneyActions {
 	
+	public String getName();
+	public int getPower();
+	
 	public void changeLocation(int x, int y);
-	public void  isWounded(int power);
+	public void isWounded(int power);
 	public void restorePower(int power);
 }
