3675a3a49f17c92750c18deab608ede3ae507f57
andreea-podovei <72200669+andreea-podovei@users.noreply.github.com>
2021-05-06 15:39:38 +0300

    Week 10 - Strategy & State implementations (final commit)

---
+++ Week10 - Proxy Strategy State/src/ro/ase/csie/cts/g1093/dp/state/SuperHero.java
@@ -0,0 +1,50 @@
+package ro.ase.csie.cts.g1093.dp.state;
+
+public class SuperHero {
+
+	public static final int CRITICAL_VALUE = 50;
+	public static final int WOUNDED_VALUE = 200;
+	
+	String name;
+	int lifePoints;
+	
+	MoveStateInterface moveState = new NormalState(); // --> this reference should never remain null
+	
+	public SuperHero(String name, int lifePoints) {
+		super();
+		this.name = name;
+		this.lifePoints = lifePoints;
+		this.moveState = new NormalState();
+	}
+
+	public void move() {
+		if(this.moveState != null)
+			this.moveState.moveAtDifferentLocation();
+		else
+			throw new UnsupportedOperationException();
+	}
+	
+	public void takeAHit(int points) {
+		this.lifePoints -= lifePoints;
+		if(this.lifePoints <= CRITICAL_VALUE) {
+			this.moveState = new CriticalWoundedState();
+		}else 
+		{
+			if(this.lifePoints <= WOUNDED_VALUE) {
+				this.moveState = new WoundedState();
+			}
+		}	
+	}
+	
+	public void heal(int points) {
+		this.lifePoints += lifePoints; 
+		if(this.lifePoints > WOUNDED_VALUE) {
+			this.moveState = new NormalState();
+		}else {
+			if(this.lifePoints > CRITICAL_VALUE) {
+				this.moveState = new WoundedState();
+			}
+		}
+			
+	}
+}
